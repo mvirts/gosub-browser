901a7b83dfb7914faead4c4f74277ebfc637395e
Joshua Thijssen <jthijssen@noxlogic.nl>
2023-08-17 09:10:35 +0200

    up

--- src/html5_parser/tokenizer.rs
+++ src/html5_parser/tokenizer.rs
@@ -17,12 +17,12 @@ pub enum Error {
 
 // The tokenizer will read the input stream and emit tokens that can be used by the parser.
 pub struct Tokenizer<'a> {
-    pub stream: &'a mut InputStream, // HTML character input stream
-    pub state: State,                // Current state of the tokenizer
-    pub consumed: Vec<char>,         // Current consumed characters for current token
-                                     // pub emitter: &'a mut dyn Emitter,   // Emitter trait that will emit the tokens during parsing
-
-    pub token_queue: Vec<Token>,     // Queue of emitted tokens. Needed because we can generate multiple tokens during iteration
+    pub stream: &'a mut InputStream,    // HTML character input stream
+    pub state: State,                   // Current state of the tokenizer
+    pub consumed: Vec<char>,            // Current consumed characters for current token
+                                        // pub emitter: &'a mut dyn Emitter,   // Emitter trait that will emit the tokens during parsing
+    pub current_token: Option<Token>,   // Token that is currently in the making (if any)
+    pub token_queue: Vec<Token>,        // Queue of emitted tokens. Needed because we can generate multiple tokens during iteration
 }
 
 impl<'a> Tokenizer<'a> {
@@ -31,7 +31,7 @@ impl<'a> Tokenizer<'a> {
             stream: input,
             state: State::DataState,
             consumed: vec![],
-            // emitter,
+            current_token: None,
             token_queue: vec![],
         };
     }
@@ -75,75 +75,249 @@ impl<'a> Tokenizer<'a> {
                 }
                 State::CharacterReferenceInDataState => {
                     // consume character reference
-                    if self.consume_character_reference(None, false).is_err() {
-                        // @TODO: something went wrong? What to do?
-                    }
+                    self.consume_character_reference(None, false);
                     self.state = State::DataState;
                 }
-                State::RcDataState => {}
-                State::CharacterReferenceInRcDataState => {}
-                State::RawTextState => {}
-                State::ScriptDataState => {}
-                State::PlaintextState => {}
-                State::TagOpenState => {}
-                State::EndTagOpenState => {}
-                State::TagNameState => {}
-                State::RcDataLessThanSignState => {}
-                State::RcDataEndTagOpenState => {}
-                State::RcDataEndTagNameState => {}
-                State::RawTextLessThanSignState => {}
-                State::RawTextEndTagOpenState => {}
-                State::RawTextEndTagNameState => {}
-                State::ScriptDataLessThenSignState => {}
-                State::ScriptDataEndTagOpenState => {}
-                State::ScriptDataEndTagNameState => {}
-                State::ScriptDataEscapeStartState => {}
-                State::ScriptDataEscapeStartDashState => {}
-                State::ScriptDataEscapedState => {}
-                State::ScriptDataEscapedDashState => {}
-                State::ScriptDataEscapedLessThanSignState => {}
-                State::ScriptDataEscapedEndTagOpenState => {}
-                State::ScriptDataEscapedEndTagNameState => {}
-                State::ScriptDataDoubleEscapeStartState => {}
-                State::ScriptDataDoubleEscapedState => {}
-                State::ScriptDataDoubleEscapedDashState => {}
-                State::ScriptDataDoubleEscapedDashDashState => {}
-                State::ScriptDataDoubleEscapedLessThanSignState => {}
-                State::ScriptDataDoubleEscapeEndState => {}
-                State::BeforeAttributeNameState => {}
-                State::AttributeNameState => {}
-                State::BeforeAttributeValueState => {}
-                State::AttributeValueDoubleQuotedState => {}
-                State::AttributeValueSingleQuotedState => {}
-                State::AttributeValueUnquotedState => {}
-                State::CharacterReferenceInAttributeValueState => {}
-                State::AfterAttributeValueQuotedState => {}
-                State::SelfClosingStartState => {}
-                State::BogusCommentState => {}
-                State::MarkupDeclarationOpenState => {}
-                State::CommentStartState => {}
-                State::CommentStartDashState => {}
-                State::CommentState => {}
-                State::CommentEndDashState => {}
-                State::CommentEndState => {}
-                State::CommentEndBangState => {}
-                State::DocTypeState => {}
-                State::BeforeDocTypeNameState => {}
-                State::DocTypeNameState => {}
-                State::AfterDocTypeNameState => {}
-                State::AfterDocTypePublicKeywordState => {}
-                State::BeforeDocTypePublicIdentifierState => {}
-                State::DocTypePublicIdentifierDoubleQuotedState => {}
-                State::DocTypePublicIdentifierSingleQuotedState => {}
-                State::AfterDoctypePublicIdentifierState => {}
-                State::BetweenDocTypePublicAndSystemIdentifiersState => {}
-                State::AfterDocTypeSystemKeywordState => {}
-                State::BeforeDocTypeSystemIdentifiedState => {}
-                State::DocTypeSystemIdentifierDoubleQuotedState => {}
-                State::DocTypeSystemIdentifierSingleQuotedState => {}
-                State::AfterDocTypeSystemIdentifiedState => {}
-                State::BogusDocTypeState => {}
-                State::CDataSectionState => {}
+                State::RcDataState => {
+                    let c = match self.stream.read_char() {
+                        Some(c) => c,
+                        None => {
+                            self.parse_error("EOF");
+                            self.token_queue.push(Token::EofToken);
+                            return;
+                        }
+                    };
+
+                    match c {
+                        '&' => self.state = State::CharacterReferenceInRcDataState,
+                        '<' => self.state = State::RcDataLessThanSignState,
+                        '\u{0000}' => {
+                            self.parse_error("NUL encountered in stream");
+                        }
+                        _ => self.consume(c),
+                    }
+                }
+                State::CharacterReferenceInRcDataState => {
+                    // consume character reference
+                    self.consume_character_reference(None, false);
+                    self.state = State::RcDataState;
+                }
+                State::RawTextState => {
+                    let c = match self.stream.read_char() {
+                        Some(c) => c,
+                        None => {
+                            self.parse_error("EOF");
+                            self.token_queue.push(Token::EofToken);
+                            return;
+                        }
+                    };
+
+                    match c {
+                        '<' => self.state = State::RawTextLessThanSignState,
+                        '\u{0000}' => {
+                            self.parse_error("NUL encountered in stream");
+                            self.consume(CHAR_REPLACEMENT);
+                            return;
+                        }
+                        _ => self.consume(c),
+                    }
+                }
+                State::ScriptDataState => {
+                    let c = match self.stream.read_char() {
+                        Some(c) => c,
+                        None => {
+                            self.parse_error("EOF");
+                            self.token_queue.push(Token::EofToken);
+                            return;
+                        }
+                    };
+
+                    match c {
+                        '<' => self.state = State::ScriptDataLessThenSignState,
+                        '\u{0000}' => {
+                            self.parse_error("NUL encountered in stream");
+                            self.consume(CHAR_REPLACEMENT);
+                            return;
+                        }
+                        _ => self.consume(c),
+                    }
+                }
+                State::PlaintextState => {
+                    let c = match self.stream.read_char() {
+                        Some(c) => c,
+                        None => {
+                            self.parse_error("EOF");
+                            self.token_queue.push(Token::EofToken);
+                            return;
+                        }
+                    };
+
+                    match c {
+                        '\u{0000}' => {
+                            self.parse_error("NUL encountered in stream");
+                            self.consume(CHAR_REPLACEMENT);
+                            return;
+                        }
+                        _ => self.consume(c),
+                    }
+                }
+                State::TagOpenState => {
+                    self.current_token = Some(Token::StartTagToken{
+                        name: String::new(),
+                        is_self_closing: false,
+                        attributes: vec![],
+                    });
+
+                    let c = match self.stream.read_char() {
+                        Some(c) => c,
+                        None => {
+                            self.parse_error("EOF");
+                            self.token_queue.push(Token::EofToken);
+                            return;
+                        }
+                    };
+
+                    match c {
+                        '!' => self.state = State::MarkupDeclarationOpenState,
+                        '/' => self.state = State::EndTagOpenState,
+                        'A'..='Z' => {
+
+                        },
+                        'a'..='z' => {
+
+                        }
+                        '?' => {
+                            self.parse_error("questionmark encountered. bogus.");
+                            self.state = State::BogusCommentState;
+                        }
+                        _ => {
+                            self.parse_error("parse error");
+                            self.consume('<');
+                            self.stream.unread();
+                            self.state = State::DataState;
+                        }
+                    }
+                }
+                State::EndTagOpenState => {
+                    let c = match self.stream.read_char() {
+                        Some(c) => c,
+                        None => {
+                            self.parse_error("EOF");
+                            self.token_queue.push(Token::EofToken);
+                            return;
+                        }
+                    };
+
+                    match c {
+                        'A'..='Z' => {
+                            // consume lower case
+                            self.consume(c as u8 - 0x20);
+                            self.state = State::TagNameState;
+                        },
+                        'a'..='z' => {
+                            self.consume(c);
+                            self.state = State::TagNameState;
+                        }
+                        '>' => {
+                            self.parse_error("> encountered");
+                            self.state = State::DataState;
+                        }
+                        _ => {
+                            self.state = State::BogusCommentState;
+                        }
+                    }
+                }
+                State::TagNameState => {
+                    let c = match self.stream.read_char() {
+                        Some(c) => c,
+                        None => {
+                            self.parse_error("EOF");
+                            self.token_queue.push(Token::EofToken);
+                            return;
+                        }
+                    };
+
+                    match c {
+                        CHAR_TAB | CHAR_LF | CHAR_FF | CHAR_SPACE => self.state = State::BeforeAttributeNameState,
+                        '/' => self.state = State::SelfClosingStartState,
+                        '>' => {
+                            self.current_token.name = self.get_consumed_str();
+                            self.token_queue.push(*self.current_token);
+                            self.current_token = None;
+
+                            self.state = State::DataState;
+                        },
+                        '\u{0000}' => {
+                            self.parse_error("NUL encountered in stream");
+                            self.consume(CHAR_REPLACEMENT);
+                        },
+                        'A'..='Z' => {
+                            self.consume(c - 0x20);
+                        }
+                        _ => self.consume(c),
+                    };
+                }
+                _ => {
+                    panic!("state {} not implemented", self.state);
+                }
+                // State::RcDataLessThanSignState => {}
+                // State::RcDataEndTagOpenState => {}
+                // State::RcDataEndTagNameState => {}
+                // State::RawTextLessThanSignState => {}
+                // State::RawTextEndTagOpenState => {}
+                // State::RawTextEndTagNameState => {}
+                // State::ScriptDataLessThenSignState => {}
+                // State::ScriptDataEndTagOpenState => {}
+                // State::ScriptDataEndTagNameState => {}
+                // State::ScriptDataEscapeStartState => {}
+                // State::ScriptDataEscapeStartDashState => {}
+                // State::ScriptDataEscapedState => {}
+                // State::ScriptDataEscapedDashState => {}
+                // State::ScriptDataEscapedLessThanSignState => {}
+                // State::ScriptDataEscapedEndTagOpenState => {}
+                // State::ScriptDataEscapedEndTagNameState => {}
+                // State::ScriptDataDoubleEscapeStartState => {}
+                // State::ScriptDataDoubleEscapedState => {}
+                // State::ScriptDataDoubleEscapedDashState => {}
+                // State::ScriptDataDoubleEscapedDashDashState => {}
+                // State::ScriptDataDoubleEscapedLessThanSignState => {}
+                // State::ScriptDataDoubleEscapeEndState => {}
+                // State::BeforeAttributeNameState => {}
+                // State::AttributeNameState => {}
+                // State::BeforeAttributeValueState => {}
+                // State::AttributeValueDoubleQuotedState => {}
+                // State::AttributeValueSingleQuotedState => {}
+                // State::AttributeValueUnquotedState => {}
+                // State::CharacterReferenceInAttributeValueState => {}
+                // State::AfterAttributeValueQuotedState => {}
+                // State::SelfClosingStartState => {
+                //
+                // }
+                // State::BogusCommentState => {}
+                // State::MarkupDeclarationOpenState => {}
+                // State::CommentStartState => {}
+                // State::CommentStartDashState => {}
+                // State::CommentState => {}
+                // State::CommentEndDashState => {}
+                // State::CommentEndState => {}
+                // State::CommentEndBangState => {}
+                // State::DocTypeState => {}
+                // State::BeforeDocTypeNameState => {}
+                // State::DocTypeNameState => {}
+                // State::AfterDocTypeNameState => {}
+                // State::AfterDocTypePublicKeywordState => {}
+                // State::BeforeDocTypePublicIdentifierState => {}
+                // State::DocTypePublicIdentifierDoubleQuotedState => {}
+                // State::DocTypePublicIdentifierSingleQuotedState => {}
+                // State::AfterDoctypePublicIdentifierState => {}
+                // State::BetweenDocTypePublicAndSystemIdentifiersState => {}
+                // State::AfterDocTypeSystemKeywordState => {}
+                // State::BeforeDocTypeSystemIdentifiedState => {}
+                // State::DocTypeSystemIdentifierDoubleQuotedState => {}
+                // State::DocTypeSystemIdentifierSingleQuotedState => {}
+                // State::AfterDocTypeSystemIdentifiedState => {}
+                // State::BogusDocTypeState => {}
+                // State::CDataSectionState => {}
             }
         }
 
@@ -233,6 +407,32 @@ mod tests {
 
         let mut tkznr = Tokenizer::new(&mut is);
 
+        let t = tkznr.next_token();
+        assert_eq!(TokenType::TextToken, t.type_of());
+
+        if let Token::TextToken { value } = t {
+            assert_eq!("This code is © 2023 €", value);
+        }
+
+        let t = tkznr.next_token();
+        assert_eq!(TokenType::EofToken, t.type_of());
+    }
+
+    #[test]
+    fn test_tags() {
+        let mut is = InputStream::new();
+        is.read_from_str("<a> <b> <foo> <FOO> <bar > <bar/> <  bar >", None);
+
+        let mut tkznr = Tokenizer::new(&mut is);
+
+        let t = tkznr.next_token();
+        let t = tkznr.next_token();
+        let t = tkznr.next_token();
+        let t = tkznr.next_token();
+        let t = tkznr.next_token();
+        let t = tkznr.next_token();
+        let t = tkznr.next_token();
+        let t = tkznr.next_token();
         let t = tkznr.next_token();
         assert_eq!(TokenType::TextToken, t.type_of());
 
